04cf7816fa95dbba3ec8bc2120716d4104c99be7
= <=>
2021-09-30 16:14:39 -0700

    A simple calculator developed using C#.NET Programming language. It can only perform operation on 2 operands at a time

---
+++ Calculator/Calculator/Calculation.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Calculation
+    {
+        bool result = false;
+        private string value = "";
+        private List<string> opr = new List<string>()
+        {
+            "+", "-", "*", "/"
+        };
+        public void setValue(string arg)
+        {
+            this.value += arg;
+        }
+        public string getValue()
+        {
+            return this.value;
+        }
+        public string ResetValue()
+        {
+            this.value = "";
+            return "0";
+        }
+        public void deleteValue()
+        {
+            if(this.value.Length > 0)
+                this.value.Remove(value.Length - 1);
+        }
+        public bool CheckOperator(string oprt)
+        {
+            foreach(string oprs in opr)
+            {
+                if(oprs == oprt)
+                {
+                    result = true;
+                }else if(oprt == ".")
+                {
+                    result = false;
+                }
+            }
+            return result;
+        }
+
+        public string ThrowError(string error)
+        {
+            return error;
+        }
+        public double getResult(string val, char opr)
+        {
+            if (opr == '√')
+            {
+                double num1 = Convert.ToDouble(val);
+                return Math.Sqrt(num1);
+            }
+            else if (opr == '²')
+            {
+                double num1 = Convert.ToDouble(val);
+                return num1*num1;
+            }
+            else {
+                string[] nums = value.Split(opr);
+                double num1 = Convert.ToDouble(nums[0]);
+                double num2 = Convert.ToDouble(nums[1]);
+                try
+                {
+                    switch (opr)
+                    {
+                        case '+':
+                            return num1 + num2;
+                            break;
+                        case '-':
+                            return num1 - num2;
+                            break;
+                        case '*':
+                            return num1 * num2;
+                            break;
+                        case '/':
+                            return num1 / num2;
+                            break;
+                        default:
+                            return 0;
+                            break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    return 0;
+                }
+            }
+        }
+    }
+}
---
+++ Calculator/Calculator/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        Calculation calc = new Calculation();
+        char ops, op;
+        bool allowDecimal, valset = true, opera = false;
+         private List<string> opr = new List<string>()
+        {
+            "+", "-", "*", "/"
+        };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btn_click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            bool strVal = true;
+            if (btn.Text == "√" || btn.Text == "²")
+            {
+                strVal = false;
+            }
+            bool seen = false;
+            //Check if operator is clicked
+            foreach (string oprs in opr)
+            {
+                if (btn.Text == oprs)
+                {
+                    opera = true;
+                    ops = Convert.ToChar(btn.Text);
+                }
+                if (calc.getValue().Contains(oprs))
+                {
+                    seen = true;
+                    op = Convert.ToChar(oprs);
+                }
+
+                if ((btn.Text == oprs || btn.Text == ".") && calc.getValue().Length == 0)
+                {
+                    calc.setValue("0");
+                }
+            }
+            /** Ends here **/
+            if (seen && opera && valset)
+            {
+                this.Compute("val", op);
+            }
+            //Check for an operator before square root
+            else if (!strVal && seen && valset)
+            {
+                this.Compute("val", op);
+                if(btn.Text == "²")
+                    this.Compute(this.textBox1.Text, '²');
+                if (btn.Text == "√")
+                    this.Compute(this.textBox1.Text, '√');
+            }
+            else if (!strVal && valset)
+            {
+                if (calc.getValue() == "")
+                    calc.setValue("0");
+
+                if (btn.Text == "²")
+                    this.Compute(this.textBox1.Text, '²');
+                if (btn.Text == "√")
+                    this.Compute(this.textBox1.Text, '√');
+            }
+
+            if (strVal)
+            {
+                //Check reoccurrence of operator
+                foreach (string oprs in opr)
+                {
+                    if (opera && this.textBox1.Text.Contains(oprs) && !valset)
+                    {
+                        string nw = calc.getValue().Remove(calc.getValue().Length - 1);
+                        calc.ResetValue();
+                        calc.setValue(nw);
+                    }
+                }
+                /** Ends here **/
+                if (!allowDecimal && btn.Text == "." && this.textBox1.Text.Contains(".")) ;
+                else
+                {
+                    if (opera)
+                    {
+                        valset = false;
+                        opera = false;
+                    }
+                    else if(!valset && btn.Text == ".")
+                        valset = false;
+                    else
+                        valset = true;
+
+                    calc.setValue(btn.Text);
+                    this.textBox1.Text = calc.getValue();
+                }
+
+                //Allow decimal point after operator
+                if (calc.CheckOperator(btn.Text))
+                    allowDecimal = true;
+                else
+                    allowDecimal = false;
+            }
+        }
+
+        private void Compute(string arg, char arg1)
+        {
+            this.textBox1.Text = calc.getResult(arg, arg1).ToString();
+            calc.ResetValue();
+            calc.setValue(this.textBox1.Text);
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            int a = 1;
+            foreach (string oprs in opr)
+            {
+                if (calc.getValue().Contains(oprs))
+                {
+                    a = 0;
+                }
+            }
+
+            if (!valset || (!valset && !allowDecimal))
+            {
+                calc.ResetValue();
+                this.textBox1.Text = calc.ThrowError("Syntax error");
+            }
+            else if (valset && a == 1) ;
+
+            else
+                this.Compute("val", ops);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = calc.ResetValue();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (calc.getValue().Length > 1)
+            {
+                string nw = calc.getValue().Remove(calc.getValue().Length - 1);
+                calc.ResetValue();
+                calc.setValue(nw);
+                this.textBox1.Text = calc.getValue();
+            }
+            else
+            {
+                this.textBox1.Text = calc.ResetValue();
+            }
+        }
+    }
+}
